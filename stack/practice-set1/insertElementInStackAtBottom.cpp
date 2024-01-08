317a337bd5fa13a43e11d18a4f97aec97641c38b
Manoj Santra <santramanoj1997@gmail.com>
2024-01-08 11:24:02 +0530

    insert element at bottom of stack

---
+++ stack/practice-set1/insertElementInStackAtBottom.cpp
@@ -0,0 +1,54 @@
+
+#include <iostream>
+#include<vector>
+#include<utility>
+#include<algorithm>
+#include<stack>
+using namespace std;
+
+void solve(stack<int>& stk, int num) {
+	if (stk.empty()) {
+		stk.push(num);
+		return;
+	}
+	int element = stk.top();
+	stk.pop();
+	solve(stk, num);
+	stk.push(element);
+}
+
+void printStackFromBottom(stack<int>& stk) {
+	if (stk.empty()) return;
+	int element = stk.top();
+	stk.pop();
+	printStackFromBottom(stk);
+	cout << element << " , ";
+	stk.push(element);
+}
+
+
+void insertAtBottomInStack(stack<int>& stk, int num) {
+	solve(stk, num);
+}
+
+
+int main()
+{
+	stack<int>stk;
+	stk.push(10);
+	stk.push(20);
+	stk.push(30);
+	stk.push(40);
+	stk.push(50);
+	stk.push(60);
+	cout<<endl << "Stack element : " << endl;
+	printStackFromBottom(stk);
+	insertAtBottomInStack(stk, 100);
+	cout << endl << "Stack element : " << endl;
+	printStackFromBottom(stk);
+
+	
+	
+}
+
+
